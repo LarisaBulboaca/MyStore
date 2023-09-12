8184a4a7ba02c7b892499c61ba4663796f51fb78
LarisaBulboaca <bulboaca.vl@gmail.com>
2023-09-12 20:43:27 +0300

    Add project files.

---
+++ MyStore/Controllers/CategoriesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Domain;
+
+namespace MyStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public CategoriesController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Category> Get() 
+        { 
+            var allCategories = context.Categories.ToList();
+            return allCategories;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetById(int id)
+        {
+            var category = context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Category> Update(int id, Category category)
+        {
+            //verificam in db daca avem ceva cu id-ul respectiv
+            //updatam daca gasim
+            //returnam 404 daca nu gasim
+
+            var existingCategory = context.Categories.Find(id);
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            TryUpdateModelAsync(existingCategory);
+            context.Categories.Update(category);
+            context.SaveChanges();
+            
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var category = context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound(category);
+            }
+              
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            //exista?
+            //stergem
+            //returnam NotFound
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category categoryToAdd)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // TODO: business rules
+
+            var addedEntity = context.Add(categoryToAdd);
+            context.SaveChanges();
+
+            //return Ok(categoryToAdd);
+
+            return CreatedAtAction(nameof(GetById), new { id = categoryToAdd.Categoryid }, categoryToAdd);
+        }
+    }
+}
---
+++ MyStore/Controllers/ShipperController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Domain;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipperController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ShipperController (StoreContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<ShipperController>
+        [HttpGet]
+        public IEnumerable<Shipper> Get()
+        {
+            var allShippers = context.Shippers.ToList();
+            return allShippers;
+       
+        }
+
+        // GET api/<ShipperController>/5
+        [HttpGet("{id}")]
+        public Shipper? GetById(int id)
+        {
+            var shipper = context.Shippers.Find(id);
+            return shipper; 
+        }
+        // PUT api/<ShipperController>/5
+
+        [HttpPut("{id}")]
+        public Shipper Update(int id, Shipper shipper)
+        {
+            var existingShipper = context.Shippers.Find(id);
+            if (existingShipper != null)
+            {
+                TryUpdateModelAsync(existingShipper);
+                context.Shippers.Update(shipper);
+                context.SaveChanges();
+            }
+            return shipper;
+        }
+
+        // POST api/<ShipperController>
+        [HttpPost]
+        public Shipper Create(Shipper shipperToAdd)
+        {
+            context.Add(shipperToAdd);
+            context.SaveChanges();
+
+            return shipperToAdd;
+        }
+
+        // DELETE api/<ShipperController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
